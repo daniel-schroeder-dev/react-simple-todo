ddc86d967baac0977171030d2d1907f2663f7b66
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-15 23:24:29 -0500

    Updates

---
+++ precache-manifest.cee18278a374da87606e7029903fbed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fec608fea9243d47efb8b07f9553fd9",
+    "url": "/react-simple-todo/index.html"
+  },
+  {
+    "revision": "66728df7aec8eb9721d2",
+    "url": "/react-simple-todo/static/css/main.260dcc90.chunk.css"
+  },
+  {
+    "revision": "d7202aa777f1d26cfd54",
+    "url": "/react-simple-todo/static/js/2.2e5cac7b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-simple-todo/static/js/2.2e5cac7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66728df7aec8eb9721d2",
+    "url": "/react-simple-todo/static/js/main.84ca1ab8.chunk.js"
+  },
+  {
+    "revision": "d4305ddf377e28427b5d",
+    "url": "/react-simple-todo/static/js/runtime-main.be9cd906.js"
+  }
+]);
